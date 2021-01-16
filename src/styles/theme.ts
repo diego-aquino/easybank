a46587a059de997d0e605ac298c97f9c69a5a74e
Diego Aquino <diego.cruz001@gmail.com>
2021-01-16 17:08:04 -0300

    Add styling resources to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,30 @@
+const baseColors = {
+  primary: {
+    darkBlue: '#2d314d',
+    limeGreen: '#31d35c',
+    brightCyan: '#2bb7da',
+  },
+  neutral: {
+    grayishBlue: '#9698a6',
+    lightGrayishBlue: '#f3f4f6',
+    lightestGray: '#fafafa',
+    white: '#ffffff',
+  },
+};
+
 const theme = {
-  colors: {},
+  colors: baseColors,
+  gradients: {
+    primary: `linear-gradient(
+      90deg,
+      ${baseColors.primary.limeGreen} 0%,
+      ${baseColors.primary.brightCyan} 100%
+    )`,
+  },
+  transitions: {
+    fast: '0.1s',
+    regular: '0.2s',
+  },
 };
 
 export default theme;
