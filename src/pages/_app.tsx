aff5e958305f9e2fa73d9d400e1a5cba9c8013b1
Diego Aquino <diego.cruz001@gmail.com>
2021-01-16 10:58:13 -0300

    Add TailwindCSS to the project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import { AppProps } from 'next/app';
 
+import 'styles/global.css';
+
 const App: FC<AppProps> = ({ Component, pageProps }) => (
   <Component {...pageProps} />
 );
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./src/pages/**/*.ts*', './src/components/**/*.ts*'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
