736c10ca323157e55b2827abcf12c6beb2df1d45
Diego Aquino <diego.cruz001@gmail.com>
2021-01-17 14:27:24 -0300

    Add social media icons

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,3 +1,15 @@
 import Logo from './Logo';
+import YouTubeIcon from './YouTubeIcon';
+import TwitterIcon from './TwitterIcon';
+import PinterestIcon from './PinterestIcon';
+import InstagramIcon from './InstagramIcon';
+import FacebookIcon from './FacebookIcon';
 
-export { Logo };
+export {
+  Logo,
+  YouTubeIcon,
+  TwitterIcon,
+  PinterestIcon,
+  InstagramIcon,
+  FacebookIcon,
+};
