d418431b05a648994cd709e8674f9945d9744e9f
Diego Aquino <diego.cruz001@gmail.com>
2021-01-17 14:29:23 -0300

    Create page wrapper component (Layout)

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -1,5 +1,6 @@
+import Layout from './Layout';
 import Header from './Header';
 import Footer from './Footer';
 import ExternalLink from './ExternalLink';
 
-export { Header, Footer, ExternalLink };
+export { Layout, Header, Footer, ExternalLink };
---
+++ src/styles/components/common/Layout.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  padding: 0 4rem;
+
+  display: flex;
+  flex-direction: column;
+
+  > main {
+    flex: 1;
+  }
+`;
