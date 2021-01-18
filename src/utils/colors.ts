c37edfb44f6a730eee779f80e86fd71d66174220
Diego Aquino <diego.cruz001@gmail.com>
2021-01-18 17:31:32 -0300

    Create utility to change the opacity of hex colors

---
+++ src/utils/colors.ts
@@ -0,0 +1,20 @@
+function expandHexColorCode(hexColorCode: string): string {
+  const [red, green, blue] = Array.from(hexColorCode);
+  const expandedHexColorCode = [red, green, blue]
+    .map((value) => value.repeat(2))
+    .join('');
+
+  return expandedHexColorCode;
+}
+
+export function toOpacity(hexColor: string, alpha = 1): string {
+  const isShortRGB = hexColor.length === 3;
+  const expandedRGBColor = isShortRGB ? expandHexColorCode(hexColor) : hexColor;
+
+  const alphaInHexadecimal = Math.floor(255 * alpha).toString(16);
+  const shouldHaveLeadingZero = alphaInHexadecimal.length === 1;
+  const formattedHexAlpha =
+    (shouldHaveLeadingZero ? '0' : '') + alphaInHexadecimal;
+
+  return expandedRGBColor + formattedHexAlpha;
+}
