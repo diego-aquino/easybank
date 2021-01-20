5bbbab33ca79f25d382cfec6b0d9e1efb0c2ade4
Diego Aquino <diego.cruz001@gmail.com>
2021-01-20 18:03:34 -0300

    Implement responsive landing page hero

--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -1,25 +1,119 @@
 import styled from 'styled-components';
-import theme from 'styles/theme';
 
-export const Container = styled.div`
-  padding: 20rem 0;
+import { Layout as RawLayout } from 'components/common';
+import { Container as Header } from 'styles/components/common/Header';
+import { Section } from 'components/home';
+import { Title, Description } from 'styles/components/home/Section';
 
-  text-align: center;
-  font-style: italic;
+export const Layout = styled(RawLayout)`
+  width: 100%;
+  min-width: 230px;
+  overflow-x: hidden;
 
+  ${Header} {
+    z-index: 1;
+  }
+`;
+
+export const Hero = styled(Section)`
   position: relative;
 
-  opacity: 0.5;
+  padding-bottom: 125.5rem;
+  margin-bottom: -80rem;
 
-  ::after {
-    content: '';
+  ${Title} {
+    font-size: 14rem;
+  }
+
+  ${Description} {
+    margin-bottom: 9rem;
+  }
+
+  @media (max-width: 768px) {
+    padding-top: 80rem;
+
+    ${Title} {
+      font-size: 12.5rem;
+    }
+  }
+`;
+
+export const HeroContent = styled.div`
+  max-width: 115rem;
+  margin-top: 15rem;
+
+  @media (max-width: 768px) {
+    margin-left: auto;
+    margin-right: auto;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    text-align: center;
+  }
+`;
+
+export const HeroImages = styled.div`
+  width: fit-content;
+  position: absolute;
+  top: 0;
+  left: 45%;
+
+  > :nth-child(1) {
     position: absolute;
-    top: 0;
-    left: calc((100% - 100vw) / 2);
-    width: 100vw;
-    height: 100%;
-    z-index: -1;
+    top: -65rem;
+    left: 0;
+  }
 
-    background-color: ${theme.colors.neutral.lightGrayishBlue};
+  > :nth-child(2) {
+    position: absolute !important;
+    top: -30rem;
+    left: 45rem;
+  }
+
+  @media (max-width: 1024px) {
+    left: 50%;
+  }
+
+  @media (max-width: 940px) {
+    left: 55%;
+  }
+
+  @media (max-width: 850px) {
+    left: 60%;
+  }
+
+  @media (max-width: 768px) {
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+
+    > *:nth-child(1) {
+      position: absolute;
+      top: unset;
+      bottom: 50rem;
+      left: 0;
+    }
+
+    > :nth-child(2) {
+      position: absolute !important;
+      top: unset;
+      bottom: 60rem;
+      left: 4rem;
+    }
+  }
+
+  @media (max-width: 628px) {
+    > *:nth-child(1) {
+      bottom: 40rem;
+    }
+  }
+
+  @media (max-width: 540px) {
+    > *:nth-child(1) {
+      bottom: 30rem;
+    }
   }
 `;
